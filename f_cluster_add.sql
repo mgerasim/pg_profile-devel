6fe0b4c348655b121b1a7fec03be6f0c1cddd049
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-03-03 08:01:42 +1000

    #2 Добавлений версий функция для значений по умолчанию

--- f_cluster_add.sql
+++ f_cluster_add.sql
@@ -1,9 +1,16 @@
+-- Добавление кластера баз данных
+-- connect_host - Хост кластера базы данных
+-- connect_port - Порт для подключения к кластеру базы данных
+-- connect_database - Имя базы данных для начального подключения к кластеру
+-- username - Пользователь для подключения к кластеру базе данных
+-- password - Пароль для пользователя 
+-- description - Описание кластера базы данных
 CREATE OR REPLACE FUNCTION cluster_add(IN connect_host varchar, 
 									IN connect_port integer, 
 									IN connect_database varchar, 
-									IN description text, 
 									IN username varchar, 
-									IN password varchar) RETURNS bigint
+									IN password varchar, 
+									IN description text) RETURNS bigint
 AS $$
 DECLARE
     ID    bigint;
@@ -11,15 +18,15 @@ BEGIN
 	INSERT INTO clusters( 	connect_host, 
 						connect_port, 
 						connect_database, 
-						description, 
 						username, 
-						password) 
+						password, 
+						description) 
 			VALUES (	connect_host, 
 						connect_port,
 						connect_database,
-						description,
 						username,
-						password)
+						password,
+						description)
 			RETURNING cluster_id 
 			INTO ID;
 			
@@ -31,6 +38,97 @@ $$ LANGUAGE plpgsql;
 COMMENT ON FUNCTION cluster_add(IN connect_host varchar, 
 									IN connect_port integer, 
 									IN connect_database varchar, 
-									IN description text, 
 									IN username varchar, 
-									IN password varchar) IS 'Добавление кластера.';
+									IN password varchar, 
+									IN description text) IS 'Добавление кластера.';
+
+-- Добавление кластера баз данных
+-- connect_host - Хост кластера базы данных
+-- connect_port - по умолчанию 5432
+-- connect_database - Имя базы данных для начального подключения к кластеру
+-- username - Пользователь для подключения к кластеру базе данных
+-- password - Пароль для пользователя 
+-- description - Описание кластера базы данных									
+CREATE OR REPLACE FUNCTION cluster_add(IN connect_host varchar, 
+									IN connect_database varchar, 
+									IN username varchar, 
+									IN password varchar, 
+									IN description text) RETURNS bigint
+AS $$
+DECLARE
+    ID    bigint;
+BEGIN
+	SELECT INTO ID cluster_add( connect_host, 
+						5432,
+						connect_database,
+						username,
+						password,
+						description);
+			
+	RETURN ID;
+				
+END;
+$$ LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION cluster_add(IN connect_host varchar, 
+									IN connect_database varchar, 
+									IN username varchar, 
+									IN password varchar, 
+									IN description text) IS 'Добавление кластера.';
+
+-- Добавление кластера баз данных
+-- connect_host - Хост кластера базы данных
+-- connect_port - По умолчанию 5432
+-- connect_database - По умолчанию postgres
+-- username - Пользователь для подключения к кластеру базе данных
+-- password - Пароль для пользователя 
+-- description - Описание кластера базы данных									
+CREATE OR REPLACE FUNCTION cluster_add(IN connect_host varchar, 
+									IN username varchar, 
+									IN password varchar, 									
+									IN description text) RETURNS bigint
+AS $$
+DECLARE
+    ID    bigint;
+BEGIN
+	SELECT INTO ID cluster_add(	connect_host, 
+						'postgres',
+						username,
+						password,
+						description);
+			
+	RETURN ID;
+				
+END;
+$$ LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION cluster_add(IN connect_host varchar, 
+									IN username varchar, 
+									IN password varchar, 									
+									IN description text) IS 'Добавление кластера.';
+									
+
+-- Добавление кластера баз данных
+-- connect_host - По умолчанию - текущий хост localhost
+-- connect_port - По умолчанию - порт 5432
+-- connect_database - По умолчанию - postgres
+-- username - По умолчанию - postgres
+-- password - Пароль пустой 
+-- description - 'Current cluster'
+CREATE OR REPLACE FUNCTION cluster_add() RETURNS bigint
+AS $$
+DECLARE
+    ID    bigint;
+BEGIN
+	SELECT INTO ID cluster_add(	'localhost', 
+						'postgres',
+						'postgres',
+						'',
+						'Current cluster`');
+			
+	RETURN ID;
+				
+END;
+$$ LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION cluster_add() IS 'Добавление кластера.';
--- test/f_cluster_add.sql
+++ test/f_cluster_add.sql
@@ -8,24 +8,26 @@ BEGIN
 
 	SELECT INTO count_before count(*) FROM clusters;
 
-	SELECT INTO ID cluster_add('test', 5361, 'dbname', 'description', 'username', 'password');
+	SELECT INTO ID cluster_add('test', 5361, 'dbname', 'username', 'password', 'description');
+	SELECT INTO ID cluster_add('test1', 'username', 'password', 'description');
+	SELECT INTO ID cluster_add('test2', 'dbname', 'username', 'password', 'description');
+	SELECT INTO ID cluster_add();
 
 	SELECT INTO count_after count(*) FROM clusters;
 	
-	IF ((count_after - count_before) = 1) THEN
-		raise notice 'Test seccess';
+	IF ((count_after - count_before) = 4) THEN
+		raise notice 'Test success';
 	ELSE
 		raise notice 'Test fail';
 	END IF;
-	
-		
+			
 	RETURN 1;
 
 			
 END
 $$ LANGUAGE plpgsql;
 
-COMMENT ON FUNCTION test_cluster_add() IS 'Тестирование функции добавления кластера.';
+COMMENT ON FUNCTION test_cluster_add() IS 'Тестирование функций добавления кластера.';
 BEGIN;
 SELECT test_cluster_add();
 ROLLBACK;
